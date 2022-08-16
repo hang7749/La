b2e02af845653ece848ab0e76fff7fd9a9322b3c
Yi Hang <yihang19990726@gmail.com>
2022-08-16 10:25:32 +0800

    locale

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocalizationController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('localization/{locale}', [LocalizationController::class, 'index']);
